ae06aab0ede2935e8a45cd0fc06d0b68039379b2
Ren <36342803+Reptop@users.noreply.github.com>
2022-05-21 21:55:18 -0700

    Update Power_Rule.cpp

--- Power_Rule.cpp
+++ Power_Rule.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
-#include <vector> 
+#include <vector>
 #include <stdio.h>
-#include <cstring> 
+#include <cstring>
 #include <map>
 #include <chrono>
 #include <stack>
 #include <sstream>
-#include <iomanip>      
-using namespace std; 
+#include <iomanip>
+using namespace std;
 
-int main() {
+int main()
+{
     cout << "Sample Input: 3x^2" << endl;
     cout << "Only Compatible with Power Rule" << endl;
     cout << "\n";
-        
-    string s; 
-    cin >> s; 
-    
-    //first get coefficient 
+
+    string s;
+    cin >> s;
+
+    // first get coefficient
     string coeffString = "";
     string expoString = "";
 
-    int i;  
-    for (i = 0; s[i] != 'x'; ++i) {
+    int i;
+    for (i = 0; s[i] != 'x'; ++i)
+    {
         coeffString.push_back(s[i]);
     }
-    //skip from the x to the stuff after the ^
-    for (i = i + 2; i != s.length(); ++i) {
+    // skip from the x to the stuff after the ^
+    for (i = i + 2; i != s.length(); ++i)
+    {
         expoString.push_back(s[i]);
     }
-        
+
     long long coeff = atof(coeffString.c_str());
     float expo = atof(expoString.c_str());
 
     cout << "Unconverted exponent: " << expoString << endl;
     cout << "Coefficient: " << coeff << endl;
-    cout << "Converted Exponent: " << expo << endl; 
+    cout << "Converted Exponent: " << expo << endl;
 
-    long long newCoeff = coeff * expo; 
+    float newCoeff = coeff * expo;
     float newExpo = expo - 1;
 
-    //exponent rounded to 2 decimals
-    cout << "Derivative is: " << newCoeff << setprecision(2) "x^" << newExpo;   
+    // exponent rounded to 2 decimals
+    cout << "Derivative is: " << newCoeff << setprecision(2) << "x^" << newExpo;
 }
